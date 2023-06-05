54b3c8013532aa9eac64280cd9e300551a24ab19
csamii <csamii27@gmail.com>
2023-06-05 15:18:27 +0000

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,18 @@
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/" + argv[1]).json()
-    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+if __name__ == '__main__':
+    # Get the employee respose
+    appEndpoint = 'https://jsonplaceholder.typicode.com'
+    userResponse = requests.get(appEndpoint + '/users/' + argv[1]).json()
 
-    completed = []
-    for todo in todos:
-        if todo.get("completed") is True:
-            completed.append(todo.get("title"))
+    # Get the sum number of tasks
+    todos = requests.get(appEndpoint + '/todos?userId=' + argv[1]).json()
 
-    print("Employee {} is done tasks with ({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    for i in completed:
-        print("\t{}".format(i))
+    # Get completed tasks plus titles
+    tasksTitle = [todo['title'] for todo in todos if todo['completed']]
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(userResponse['name'], len(tasksTitle), len(todos)))
+
+    [print('\t {}'.format(title)) for title in tasksTitle]
